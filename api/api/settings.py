f62e53500e2a45cd2e5a73b84cc640a4016a44f1
MathyouMB <matthewmacraebovell@bell.net>
2021-05-03 08:38:14 -0400

    format code

--- api/api/settings.py
+++ api/api/settings.py
@@ -84,13 +84,13 @@ WSGI_APPLICATION = "api.wsgi.application"
 # }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'community_db',
-        'USER': 'postgres',
-        'PASSWORD': '1234',
-        'HOST': 'db',
-        'PORT': 5432,
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "community_db",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
 
