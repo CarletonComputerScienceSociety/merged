7b8a1dfa7b1bdd94d45c85dc74122440152ed209
Dami Adesola <damilola.adesola1@gmail.com>
2021-05-03 19:53:36 -0400

    Changed Settings Database Back to Default

--- api/api/settings.py
+++ api/api/settings.py
@@ -73,23 +73,26 @@ TEMPLATES = [
 WSGI_APPLICATION = "api.wsgi.application"
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'db',
-#        'USER': 'postgres',
-#        'PASSWORD': '1234',
-#        'HOST': 'db',
-#        'PORT': 5432,
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
 #    }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "community_db",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        "HOST": "db",
+        "PORT": 5432,
+    }
+}
 
 if os.environ.get("GITHUB_WORKFLOW"):
     DATABASES = {
