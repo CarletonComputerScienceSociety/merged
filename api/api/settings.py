ec7e5dd5f739c3e5f03156f59a4c45d891a9ba7a
Dami Adesola <damilola.adesola1@gmail.com>
2021-05-04 13:07:49 -0400

    Updated Pagination Settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 30,
 }
 
 if os.environ.get("GITHUB_WORKFLOW"):
