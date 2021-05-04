dc35838605e50d26268fb4427f6aa6de54c90cb4
Dami Adesola <damilola.adesola1@gmail.com>
2021-05-03 23:52:05 -0400

    Implemented Page Number Pagination

--- api/api/settings.py
+++ api/api/settings.py
@@ -95,8 +95,8 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
 
 if os.environ.get("GITHUB_WORKFLOW"):
