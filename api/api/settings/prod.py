8ad888dad5d51fd8ec375d12a2c1ac1eefcee82a
Forest Anderson <forestkzanderson@gmail.com>
2021-05-07 09:58:59 -0400

    Changed default settings file (#27)

--- api/api/settings/prod.py
+++ api/api/settings/prod.py
@@ -4,7 +4,11 @@ DEBUG = False
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "community_db",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        "HOST": "db",
+        "PORT": 5432,
     }
 }
