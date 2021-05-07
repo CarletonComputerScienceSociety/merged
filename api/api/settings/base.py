94c180456e1ea8b73eda95742cf5781d3818c570
Forest Anderson <forestkzanderson@gmail.com>
2021-05-06 20:25:53 -0400

    Changed default settings file

--- api/api/settings/base.py
+++ api/api/settings/base.py
@@ -73,27 +73,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "api.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.sqlite3",
-#        "NAME": BASE_DIR / "db.sqlite3",
-#    }
-# }
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "community_db",
-        "USER": "postgres",
-        "PASSWORD": "1234",
-        "HOST": "db",
-        "PORT": 5432,
-    }
-}
-
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 30,
