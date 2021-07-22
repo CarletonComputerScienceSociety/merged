910cf430088df031552e401941c12d9f405a2694
MathyouMB <matthewmacraebovell@bell.net>
2021-07-22 11:07:58 -0400

    updated data format

--- client/data/events/Events.request.ts
+++ client/data/events/Events.request.ts
@@ -3,8 +3,7 @@ import { exampleEvents } from '../../data'
 
 const getEvents = async (): Promise<Event[]> => {
   // eslint-disable-next-line no-constant-condition
-  if (false) {
-    // ADD ENV VAR TO TELL US TO USE THE REAL BACKEND OR NO
+  if (false) { // ADD ENV VAR TO TELL US TO USE THE REAL BACKEND OR NO
     return fetch('http://127.0.0.1:8000/api/v1/events/', {
       headers: {
         'Content-Type': 'application/json',
--- client/ui/pages/Home/Home.tsx
+++ client/ui/pages/Home/Home.tsx
@@ -10,11 +10,10 @@ const Home = ({ data }: Props) => {
   return (
     <div>
       <h1>Events:</h1>
-      <EventCard title = "Tech Talk" date = "27 May 2021" body = "Blockchain + Crypto Q&A Session"/>
       <div className="events">
         {
-          data.map(event => (
-            <h2 key={event.id}>{event.id}. {event.title}</h2>
+          data.map((event, index) => (
+            <EventCard key={index} title={event.title} date="27 May 2021" body={event.body}/>
           ))
         }
       </div>
