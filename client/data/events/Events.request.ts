d538ca84254e010bc60768d68dc477b6820236cd
MathyouMB <matthewmacraebovell@bell.net>
2021-07-21 22:30:32 -0400

    add conditional pass of mock data

--- client/data/events/Events.request.ts
+++ client/data/events/Events.request.ts
@@ -1,12 +1,19 @@
 import { Event } from '../../model'
+import { exampleEvents } from '../../data'
 
 const getEvents = async (): Promise<Event[]> => {
-  return fetch('http://127.0.0.1:8000/api/v1/events/', {
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json'
-    }
-  }).then((response) => response.json())
+  // eslint-disable-next-line no-constant-condition
+  if (false) {
+    // ADD ENV VAR TO TELL US TO USE THE REAL BACKEND OR NO
+    return fetch('http://127.0.0.1:8000/api/v1/events/', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    }).then((response) => response.json())
+  } else {
+    return exampleEvents
+  }
 }
 
 export { getEvents }
