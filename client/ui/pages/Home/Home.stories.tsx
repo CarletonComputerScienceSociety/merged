fb699824737320e49ec10a460916cd7645a44bfa
MathyouMB <matthewmacraebovell@bell.net>
2021-07-22 12:11:39 -0400

    remove page tests and stories

--- client/ui/pages/Home/Home.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from '@storybook/react'
-
-import { Home } from './Home'
-import { exampleEvents } from '../../../data'
-
-storiesOf('Home', module).add('Page', () => {
-  return <Home data={exampleEvents}/>
-})
