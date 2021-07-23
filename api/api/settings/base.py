8c09ecd319c82221be4f9bea4b888f8533b79ca1
Dami Adesola <damilola.adesola1@gmail.com>
2021-07-23 23:53:27 +0400

    Implemented Minio S3 Integration, Updated Requirements and Added Image Tester in Event Model

--- api/api/settings/base.py
+++ api/api/settings/base.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
