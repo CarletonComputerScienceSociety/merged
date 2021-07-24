ceea75045131876610ed9cb5f351cedcc59f117a
MathyouMB <matthewmacraebovell@bell.net>
2021-07-24 18:47:15 -0400

    add missing poster migration

--- api/api/settings/base.py
+++ api/api/settings/base.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-ztki+u&!a^9hkoo4&6&3ij73g6cho1irpg#-7s4hwr4m(1i$os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 # Application definition
 
