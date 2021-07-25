44476cc941ed0fc3e00d988e99008fd9f9ab93ef
MathyouMB <matthewmacraebovell@bell.net>
2021-07-25 02:56:20 -0400

    mock organization data

--- client/data/Organizations.data.ts
+++ client/data/Organizations.data.ts
@@ -4,74 +4,86 @@ const ORGANIZATIONS: Organization[] = [
   {
     id: 1,
     title: 'Carleton Computer Science Society',
-    description: 'test',
-    imgUrl: 'https://ccss.carleton.ca/images/ccss-logo.png'
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    imgUrl: 'https://i.imgur.com/dhzJz7r.jpg',
+    backgroundUrl: 'https://ccss.carleton.ca/ccss-website/images/slideshow/orientation.jpeg'
   },
   {
     id: 2,
     title: 'CuHacking',
-    description: 'test',
-    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/goodcuHacking.png'
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/goodcuHacking.png',
+    backgroundUrl: 'https://ccss.carleton.ca/ccss-website/images/slideshow/orientation.jpeg'
   },
   {
     id: 3,
     title: 'SCESoc',
-    description: 'test',
-    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/scesoc.png'
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/scesoc.png',
+    backgroundUrl: 'https://ccss.carleton.ca/ccss-website/images/slideshow/orientation.jpeg'
   },
   {
     id: 4,
     title: 'Women in Computer Science',
-    description: 'test',
-    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/wics.png'
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/wics.png',
+    backgroundUrl: 'https://ccss.carleton.ca/ccss-website/images/slideshow/orientation.jpeg'
   },
   {
     id: 5,
     title: 'Carleton A.I. Society',
-    description: 'test',
-    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/carletonai.png'
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/carletonai.png',
+    backgroundUrl: 'https://ccss.carleton.ca/ccss-website/images/slideshow/orientation.jpeg'
   },
   {
     id: 6,
     title: 'CU Blueprint',
-    description: 'test',
-    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/blueprint.jpg'
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/blueprint.jpg',
+    backgroundUrl: 'https://ccss.carleton.ca/ccss-website/images/slideshow/orientation.jpeg'
   },
   {
     id: 7,
     title: 'Technolgap',
-    description: 'test',
-    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/technolgap.png'
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/technolgap.png',
+    backgroundUrl: 'https://ccss.carleton.ca/ccss-website/images/slideshow/orientation.jpeg'
   },
   {
     id: 8,
     title: 'Carleton Web Development Club',
-    description: 'test',
-    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/webdev.png'
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/webdev.png',
+    backgroundUrl: 'https://ccss.carleton.ca/ccss-website/images/slideshow/orientation.jpeg'
   },
   {
     id: 9,
     title: 'Carleton eggX',
-    description: 'test',
-    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/eggx-long.png'
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/eggx-long.png',
+    backgroundUrl: 'https://ccss.carleton.ca/ccss-website/images/slideshow/orientation.jpeg'
   },
   {
     id: 10,
     title: 'BITsoc',
-    description: 'test',
-    imgUrl: 'https://bitsoc.ca/assets/Logo.png'
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    imgUrl: 'https://bitsoc.ca/assets/Logo.png',
+    backgroundUrl: 'https://ccss.carleton.ca/ccss-website/images/slideshow/orientation.jpeg'
   },
   {
     id: 11,
     title: 'Carleton Cyber Security Club',
-    description: 'test',
-    imgUrl: 'https://carletoncybersecurityclub.github.io/assets/images/1.png'
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    imgUrl: 'https://carletoncybersecurityclub.github.io/assets/images/1.png',
+    backgroundUrl: 'https://ccss.carleton.ca/ccss-website/images/slideshow/orientation.jpeg'
   },
   {
     id: 12,
     title: 'Game Design Carleton',
-    description: 'test',
-    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/blueprint.jpg'
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    imgUrl: 'https://ccss.carleton.ca/ccss-website/images/blueprint.jpg',
+    backgroundUrl: 'https://ccss.carleton.ca/ccss-website/images/slideshow/orientation.jpeg'
   }
 ]
 
