05a287661f0b45f63e5fad790ab056c837c4d2d5
MathyouMB <matthewmacraebovell@bell.net>
2021-07-25 04:50:52 -0400

    fix nextjs link error

--- client/ui/shared/Navbar/Navbar.tsx
+++ client/ui/shared/Navbar/Navbar.tsx
@@ -5,18 +5,20 @@ const Navbar = () => (
   <div className="navbar">
     <div className="navbar-container">
       <Link href="/">
-        <img
-          className="navbar-image"
-          alt="logo"
-          src="https://i.imgur.com/c41MHJN.png"
-        />
+        <a>
+          <img
+            className="navbar-image"
+            alt="logo"
+            src="https://i.imgur.com/c41MHJN.png"
+          />
+        </a>
       </Link>
       <div className="navbar-items">
         <div>
-          <Link href="/about">About</Link>
+          <Link href="/about"><a>About</a></Link>
         </div>
         <div>
-          <Link href="/organizations">Organizations</Link>
+          <Link href="/organizations"><a>Organizations</a></Link>
         </div>
         <div>
           <a href="https://github.com/CarletonComputerScienceSociety/community-project">
