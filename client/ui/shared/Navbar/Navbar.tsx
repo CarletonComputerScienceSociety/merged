6457f702dc42243c46a0489fbe608abc38e9d5da
MathyouMB <matthewmacraebovell@bell.net>
2021-08-05 17:37:08 -0400

    mobile view

--- client/ui/shared/Navbar/Navbar.tsx
+++ client/ui/shared/Navbar/Navbar.tsx
@@ -1,33 +1,12 @@
 import React from 'react';
-import Link from 'next/link';
+import { DesktopNavbar } from './DesktopNavbar';
+import { MobileNavbar } from './MobileNavbar';
 
 const Navbar = () => (
-  <div className="navbar">
-    <div className="navbar-container">
-      <Link href="/">
-        <a>
-          <img
-            className="navbar-image"
-            alt="logo"
-            src="https://i.imgur.com/c41MHJN.png"
-          />
-        </a>
-      </Link>
-      <div className="navbar-items">
-        <div>
-          <Link href="/about"><a>About</a></Link>
-        </div>
-        <div>
-          <Link href="/organizations"><a>Organizations</a></Link>
-        </div>
-        <div>
-          <a href="https://github.com/CarletonComputerScienceSociety/community-project">
-            Contribute
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
+  <>
+    <DesktopNavbar />
+    <MobileNavbar />
+  </>
 );
 
 export { Navbar };
--- client/ui/shared/index.ts
+++ client/ui/shared/index.ts
@@ -3,3 +3,4 @@ export { Footer } from './Footer';
 export { EventCard } from './EventCard';
 export { Navbar } from './Navbar';
 export { OrganizationCard } from './OrganizationCard';
+export { PrimaryButton } from './PrimaryButton';
