993e159df1de8d85ac925413dc8367f2c6aeca42
MathyouMB <matthewmacraebovell@bell.net>
2021-08-15 00:23:58 -0400

    schema changes

--- api/community/models.py
+++ api/community/models.py
@@ -7,13 +7,17 @@ from django.urls import reverse
 from django.utils.text import slugify
 
 
+STATUS_CHOICES = (
+    ("draft", "DRAFT"),
+    ("public", "PUBLIC"),
+)
+
+
 class Member(models.Model):
-    first_name = models.CharField(max_length=200)  # Represents the members first name
-    last_name = models.CharField(max_length=200)  # Represents the members last name
-    title = models.CharField(max_length=150)  # Represents the members Position
-    email = models.EmailField(
-        max_length=255, unique=True, db_index=True
-    )  # Represents the members email
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    title = models.CharField(max_length=150)
+    email = models.EmailField(max_length=255, unique=True, db_index=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
@@ -21,63 +25,41 @@ class Member(models.Model):
 
 class NewsItem(PolymorphicModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(
-        max_length=100
-    )  # Represents the Title of the Announcment/Event
+    title = models.CharField(max_length=100)
+    status = models.CharField(max_length=12, choices=STATUS_CHOICES, default="public")
+    external_link = models.CharField(max_length=300, blank=True)
 
 
 class Announcement(NewsItem):
-    preview = models.TextField(
-        max_length=500
-    )  # Represents the preview data of the announcement
-    publication_date = models.DateField(
-        auto_now=False, auto_now_add=False
-    )  # Represents the publication date
-    link = models.CharField(
-        max_length=300, blank=True
-    )  # Represents Link to the main announcement
+    excerpt = models.CharField(
+        max_length=150,
+        null=True,
+    )
+    publication_date = models.DateField(auto_now=False, auto_now_add=False)
 
     def __str__(self):
         return self.title
 
 
-STATUS_CHOICES = (
-    ("finalized", "FINALIZED"),
-    ("in planning", "IN PLANNING"),
-)
-
-
 class Organization(models.Model):
-    title = models.CharField(max_length=150)  # Represents the title of the Organization
-    description = models.TextField(
-        null=True, blank=True
-    )  # Represents the description of the Organization
-    website = models.CharField(
-        max_length=300, blank=True
-    )  # Represents the organization website
-    facebook = models.CharField(
-        max_length=250, blank=True
-    )  # Represents the organization facebook
-    instagram = models.CharField(
-        max_length=250, blank=True
-    )  # Represents the organization instagram
-    discord = models.CharField(
-        max_length=250, blank=True
-    )  # Represents the organization discord
-    slack = models.CharField(
-        max_length=250, blank=True
-    )  # Represents the organization slack
-    members = models.ManyToManyField(
-        Member, blank=True
-    )  # This links various members to single organisation
-    announcements = models.ManyToManyField(
-        Announcement, blank=True
-    )  # This links various members to single organisation
+    title = models.CharField(max_length=150)
+    description = models.TextField(null=True, blank=True)
+    website = models.CharField(max_length=300, blank=True)
+    facebook = models.CharField(max_length=250, blank=True)
+    instagram = models.CharField(max_length=250, blank=True)
+    discord = models.CharField(max_length=250, blank=True)
+    slack = models.CharField(max_length=250, blank=True)
+    members = models.ManyToManyField(Member, blank=True)
+    announcements = models.ManyToManyField(Announcement, blank=True)
     slug = models.SlugField(
         default="",
         editable=False,
         unique=True,
-    )  # Represents slug field for page
+    )
+    logo = models.ImageField(upload_to="logo", null=True, blank=True)
+    profile_background = models.ImageField(
+        upload_to="profile_background", null=True, blank=True
+    )
 
     def save(self, *args, **kwargs):
         if self.slug == "":
@@ -89,26 +71,11 @@ class Organization(models.Model):
 
 
 class Event(NewsItem):
-    description = models.TextField(
-        null=True, blank=True
-    )  # Represents the event description
-    start_time = models.DateTimeField(
-        null=True, blank=True
-    )  # Represents Start time of event
-    end_time = models.DateTimeField(
-        null=True, blank=True
-    )  # Represents End time of event
-    location = models.TextField(
-        max_length=200, null=True, blank=True
-    )  # Represents location of Event
-    link = models.CharField(max_length=300, blank=True)  # Represents the event link
-    status = models.CharField(
-        max_length=12, choices=STATUS_CHOICES, default="in planning"
-    )
-    # Represents if the event is in planning or finalized
-    organization = models.ManyToManyField(
-        Organization, related_name="events"
-    )  # This links various members to single organisation
+    description = models.TextField(null=True, blank=True)
+    start_time = models.DateTimeField(null=True, blank=True)
+    end_time = models.DateTimeField(null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
+    organization = models.ManyToManyField(Organization, related_name="events")
     poster = models.ImageField(upload_to="event", null=True, blank=True)
 
     def __str__(self):
