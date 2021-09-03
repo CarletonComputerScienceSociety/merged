04c90bc8a69fc756d63e3bd6a916f3d82de21409
Forest Anderson <forestkzanderson@gmail.com>
2021-09-03 00:16:02 -0400

    Added proper prod settings

--- api/api/settings/prod.py
+++ api/api/settings/prod.py
@@ -2,6 +2,9 @@ from .base import *
 
 DEBUG = False
 
+for i in range(1, 255):
+    ALLOWED_HOSTS.append(f"192.168.30.{i}")
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
--- api/start-prod.sh
+++ api/start-prod.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export DJANGO_SETTINGS_MODULE=api.settings.prod
 gunicorn \
     -w 2 \
     -b 0.0.0.0:8000 \
