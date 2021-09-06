4ec0aa1f8cd1500569780c163e95beaca90ec468
Forest Anderson <forestkzanderson@gmail.com>
2021-09-06 11:50:25 -0400

    Switch to runserver

--- api/start-prod.sh
+++ api/start-prod.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export DJANGO_SETTINGS_MODULE=api.settings.prod
-gunicorn \
-    -w 2 \
-    -b 0.0.0.0:8000 \
-    api.wsgi:application
+python manage.py runserver
+# gunicorn \
+#     -w 2 \
+#     -b 0.0.0.0:8000 \
+#     api.wsgi:application
