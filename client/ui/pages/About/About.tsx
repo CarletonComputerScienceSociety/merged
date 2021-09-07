563212f4c92b9d740e603058f863a4f4af0d93f0
MathyouMB <matthewmacraebovell@bell.net>
2021-09-07 02:36:38 -0400

    grammar fix

--- client/ui/pages/About/About.tsx
+++ client/ui/pages/About/About.tsx
@@ -4,7 +4,7 @@ const About = () => (
   <div className="about-page">
     <div className="page-wrap" style={{marginTop: "1rem"}}>
       <h1>About Us</h1>
-      <p className="about-page-description"><b>Merged</b> is an iniative created by the Carleton Computer Science Society with the goal of creating a more accesible way of finding events within Carleton's vibrant computer science community. We currently aim to feature events from all of Carleton's technology focused clubs and societies.</p>
+      <p className="about-page-description"><b>Merged</b> is an initiative created by the Carleton Computer Science Society with the goal of creating a more accessible way of finding events within Carleton's vibrant computer science community. We currently aim to feature events from all of Carleton's technology focused clubs and societies.</p>
       <img src="/orientation.jpg" alt="csstudents"/>
     </div>
   </div>
